716ed929b8a4a69fe4310302b8756775a15759ca
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2023-08-31 02:26:16 +0100

    Change parsing to maintain the order of classes in TOML file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::cmp::Ordering;
-use std::collections::HashMap;
 use std::fs;
 use std::str::FromStr;
 use std::time::Instant;
 
 use itertools::Itertools;
+use toml::map::Map;
+use toml::Value;
+use toml::value::Array;
 
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 enum Day {
@@ -236,44 +238,50 @@ fn solve(subjects: Vec<Subject>) {
     }
     (1..=5).for_each(|days| {
         println!();
-        println!("Best timetables with {} days with classes:", days);
+        println!("Best timetables with {} days of classes:", days);
         generate_results(&result, days);
     });
 }
 
-fn load_schedule_file(file_name: &str) -> Result<Vec<Subject>, ParseError> {
-    let content = fs::read_to_string(file_name).map_err(|_| ParseError("File not found"))?;
-
-    let data: HashMap<String, Vec<HashMap<String, String>>> =
-        toml::from_str(content.as_ref()).map_err(|_| ParseError("Invalid TOML file"))?;
-
-    let mut result = Vec::new();
-
-    for (subject_name, shifts_vec) in data {
-        for shifts_map in shifts_vec {
-            let mut shifts = Vec::new();
-            for (shift_name, shift_data) in shifts_map {
-                let (day, duration) =
-                    shift_data
+fn handle_shifts_array(shifts_array: Array) -> Result<Vec<Shift>, ParseError> {
+    let mut shifts = Vec::new();
+    for shift in shifts_array {
+        if let Value::Table(shift) = shift {
+            for (shift_name, string_data) in shift {
+                if let Value::String(shift_data) = string_data {
+                    let (day, duration) = shift_data
                         .split_whitespace()
                         .collect_tuple()
                         .ok_or(ParseError(
-                            "Invalid shift data format. Expected: <day> <start>-><end>",
-                        ))?;
+                            "Invalid shift data format. Expected: <day> <start>-><end>"))?;
+                    let day = day.parse()?;
+                    let duration = duration.parse()?;
 
-                let day = day.parse()?;
-                let duration = duration.parse()?;
-
-                shifts.push(Shift {
-                    name: shift_name,
-                    day,
-                    duration,
-                })
+                    shifts.push(Shift {
+                        name: shift_name,
+                        day,
+                        duration,
+                    })
+                }
             }
+        }
+    }
+    Ok(shifts)
+}
+
+fn load_schedule_file(file_name: &str) -> Result<Vec<Subject>, ParseError> {
+    let content = fs::read_to_string(file_name).map_err(|_| ParseError("File not found"))?;
+    let data: Map<String, Value> = toml::from_str(&content).map_err(|_| ParseError("Invalid TOML file"))?;
+
+    let mut result = Vec::new();
+
+    for (subject_name, shifts_array) in data {
+        if let Value::Array(shifts_array) = shifts_array {
+            let shifts = handle_shifts_array(shifts_array)?;
             result.push(Subject {
                 name: subject_name.clone(),
                 available_shifts: shifts,
-            })
+            });
         }
     }
 
